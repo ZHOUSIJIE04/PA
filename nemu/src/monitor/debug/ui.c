a3eef9a34264cf677cdf9e9f4d4395819d1f22a4
tracer-ics2017 <tracer@njuics.org>
2025-02-26 02:26:30 -0800

    >  compile 2212126 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  02:26:30 up  6:20,  1 user,  load average: 0.12, 0.09, 0.03 3918d4e229569bb483153d3b50198ec2dc5fda05

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,79 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args){
+   int N=0;
+   if(args!=NULL){
+      N=atoi(strtok(args," "));
+      if(N<=0){
+        printf("N must be greater than 0!");  
+        return 0; 
+      }
+   }else{
+      N=1;
+   }
+   cpu_exec(N);
+   return 0;
+}
+
+static int cmd_info(char * args){
+  if(args ==NULL){
+    printf("info后面必须有输入!");
+    return 0;
+  }
+  if(strcmp(args,"r"))
+  {
+    for(int i = 0; i < 8; i++) {
+      printf("%-8s0x%08x%16d\t", regsl[i], reg_l(i), reg_l(i));
+      if(i%2==1){
+        printf("\n");
+      }
+    }
+    printf("%-8s0x%08x%16d\n", "eip", cpu.eip, cpu.eip);
+    //16bit
+    for(int i = 0; i < 8; i++) {
+      printf("%-8s0x%08x%16d\t", regsw[i], reg_w(i), reg_w(i));
+        if (i % 2 == 1)
+            printf("\n");
+    }
+    //8bit
+    for(int i = 0; i < 8; i++)
+    {
+      printf("%-8s0x%08x%16d\t", regsb[i], reg_b(i), reg_b(i));
+        if (i % 2 == 1)
+            printf("\n");
+    }
+    return 0;
+  }
+  else if(strcmp(args,"w")==0){
+    //print_wp();
+  }else{
+    printf("输入的格式存在错误，请重新输入，或输入help获取提示！");
+  }
+
+  return 0;
+}
+static int cmd_x(char *args) {
+  int cnt = 0;
+  vaddr_t addr;
+  int temp = sscanf(args, "%d 0x%x", &cnt, &addr);
+  if(temp <= 0) {
+    printf("args error\n");
+    return 0;
+  }
+  printf("[Memory:]");
+  for(int i = 0; i < cnt; i++) {
+    if(i % 4 == 0) {
+      printf("\n0x%x:  0x%02x", addr + i, vaddr_read(addr + i, 1));
+    }  
+    else {
+      printf("  0x%02x", vaddr_read(addr + i, 1));
+    }
+  }
+  printf("\n");
+  return 0;
+}
+
 static struct {
   char *name;
   char *description;
@@ -48,6 +121,9 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
 
   /* TODO: Add more commands */
+  {"si", "arguments: [N]; carry out [N] instructions sequentially", cmd_si},
+  {"info", "Print reg info or Print monitor point information", cmd_info},
+  {"x", "Scan the memory", cmd_x},
 
 };
 
@@ -114,3 +190,4 @@ void ui_mainloop(int is_batch_mode) {
     if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
   }
 }
+
