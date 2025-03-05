792b46d649f71042b8591026388c8b0d078bf3c8
tracer-ics2017 <tracer@njuics.org>
2025-03-05 16:02:39 +0800

    >  compile 2212126 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  16:02:39 up 3 min,  1 user,  load average: 0.12, 0.23, 0.11 7fe59f9da59cbd551ecad239a66b5aaa64b3325

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,7 +5,8 @@
  */
 #include <sys/types.h>
 #include <regex.h>
-
+#include <stdlib.h>
+#include <string.h>
 enum {
   TK_NOTYPE = 256, TK_EQ,
   TK_HEX,
@@ -109,7 +110,7 @@ static bool make_token(char *e) {
                break;
           case TK_NUM:
           case TK_HEX:
-          case TK_REG:
+          case TK_REG:  //考虑是否需要去掉前面的$以及0x
                if(substr_len>32){
                 printf("数字长度超过32位，无法正常识别！");
                 break;
@@ -133,27 +134,161 @@ static bool make_token(char *e) {
     }
   }
 
-  if(tokens[0].type==TK_MUL){
-    tokens[0].type=TK_DER;
-  }else if(tokens[0].type==TK_SUB){
-    tokens[0].type=TK_MINUS;
-  }
-
-  for(int j=1;j<nr_token;j++){
-    if(tokens[j].type==TK_MUL&&tokens[j-1].type!=TK_RP&&
-    (tokens[j-1].type>TK_REG||tokens[j-1].type==TK_EQ))
-    {
-      tokens[j].type=TK_DER;
-    }
-    else if(tokens[j].type==TK_SUB&&tokens[j-1].type!=TK_RP&&
-      (tokens[j-1].type>TK_REG||tokens[j-1].type==TK_EQ)){
-        tokens[j].type=TK_MINUS;
-      }
-  }
-
   return true;
 }
 
+int find_dominant_op(int p,int q){
+  int op=0;
+  int num=0;
+  int minop=6;
+  int temp=0;
+  for(int i=p;i<=q;i++){
+     if(tokens[i].type==TK_LP){
+      num++;
+      continue;
+     }else if(tokens[i].type==TK_RP){
+      num--;
+      continue;
+     }else if(num==0){
+       if(tokens[i].type==TK_NUM||tokens[i].type==TK_HEX||tokens[i].type==TK_REG||
+          tokens[i].type==TK_DER||tokens[i].type==TK_MINUS){
+          temp=0;
+          continue;
+        }else if(tokens[i].type==TK_OR){
+          temp=1;
+        }else if(tokens[i].type==TK_AND){
+          temp=2;
+        }else if(tokens[i].type==TK_EQ||tokens[i].type==TK_NEQ){
+          temp=3; 
+        }else if(tokens[i].type==TK_PLUS||tokens[i].type==TK_SUB){
+          temp=4;
+        }else if(tokens[i].type==TK_MUL||tokens[i].type==TK_DIV){
+          temp=5;
+        }
+     }
+     if(temp<=minop){//必须取等号表示相同优先级下右边的那个更低
+      minop=temp;
+      op=i;
+     }
+  }
+  return op;
+}
+
+
+bool check_parentheses(int left, int right){
+  int num=0;
+  for(int i=left;i<right;i++){
+    if(tokens[i].type==TK_LP){
+      num++;
+    }else if(tokens[i].type==TK_RP){
+      num--;
+    }
+  }
+  if(num==0){
+    return true;
+  }else {
+    return false;
+  }
+}
+uint32_t hex_to_dec(char str[32]) {
+  uint result = 0;
+  for (int i = 2; i < 10; ++i) {
+      int tmp = 0;
+      if (str[i] >= '0' && str[i] <= '9') {
+          tmp = (int)str[i] - (int)'0';
+      } else if (str[i] >= 'a' && str[i] <= 'f') {
+          tmp = (int)str[i] - (int)'a' + 10;
+      } else if (str[i] >= 'A' && str[i] <= 'F') {
+          tmp = (int)str[i] - (int)'A' + 10;
+      } else
+          break;
+      result = 16 * result + tmp;
+  }
+  return result;
+}
+
+uint32_t eval(int p,int q ){
+  if(p>q){
+    printf("error: p > q\n");
+    assert(0);
+  }
+  else if(p==q){
+     if(tokens[p].type==TK_NUM){
+      return atoi(tokens[p].str);
+     }else if(tokens[p].type==TK_HEX){
+      return hex_to_dec(tokens[p].str);
+     }else if(tokens[p].type==TK_REG){
+      char *str = tokens[p].str;
+      if (str[0] == '$') {
+          str++; // 跳过 '$' 符号
+      }
+
+      for (int i = 0; i < 8; i++) {
+          if (strcmp(str, regsl[i]) == 0) {
+              return reg_l(i);
+          }
+          if (strcmp(str, regsw[i]) == 0) {
+              return reg_w(i);
+          }
+          if (strcmp(str, regsb[i]) == 0) {
+              return reg_b(i);
+          }
+      }
+
+      if (strcmp(str, "eip") == 0) {
+          return cpu.eip;
+      } else {
+          printf("error in TK_REG in eval()\n");
+          assert(0);
+      }
+     }
+
+  }else if(p==q-1){ //单目运算符处理
+    if (tokens[p].type == TK_NOT) {
+      return !eval(q, q);
+    } else if (tokens[p].type == TK_MINUS) {
+        return -1 * eval(p + 1, q);
+    }else if (tokens[p].type == TK_DER){
+        uint32_t addr=eval(p+1,q);
+        uint32_t result=vaddr_read(addr,4);
+        return result;
+    }
+  }
+  else if(check_parentheses(p,q)==true){
+
+    return eval(p+1,q-1);
+  }
+
+  else {
+    int op = find_dominant_op(p,q);
+    uint32_t val1=eval(p,op-1);
+    uint32_t val2=eval(op+1,q);
+    
+    switch (tokens[op].type)
+    {
+      case TK_PLUS:
+      return val1 + val2;
+      case TK_SUB:
+          return val1 - val2;
+      case TK_MUL:
+          return val1 * val2;
+      case TK_DIV:
+          return val1 / val2;
+      case TK_EQ:
+          return val1 == val2;
+      case TK_NEQ:
+          return val1 != val2;
+      case TK_AND:
+          return val1 && val2;
+      case TK_OR:
+          return val1 || val2;
+      default:
+          printf("Token computing fail!\n");
+          assert(0);
+  }
+  }
+ return 0;
+}
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
@@ -161,8 +296,30 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  if(tokens[0].type==TK_MUL){
+    tokens[0].type=TK_DER;
+  }else if(tokens[0].type==TK_SUB){
+    tokens[0].type=TK_MINUS;
+  }
 
-  return 0;
+  for(int j=1;j<nr_token;j++){
+      if(tokens[j].type==TK_MUL&&tokens[j-1].type!=TK_RP&&
+        (tokens[j-1].type>TK_REG||tokens[j-1].type==TK_EQ))
+          {
+            tokens[j].type=TK_DER;
+          }
+      else if(tokens[j].type==TK_SUB&&tokens[j-1].type!=TK_RP&&
+        (tokens[j-1].type>TK_REG||tokens[j-1].type==TK_EQ))
+          {
+            tokens[j].type=TK_MINUS;
+          }
+  }
+  
+  //if (!check_parentheses(0, nr_token)) {
+    *success = true;
+  //   return 0;
+  // } else {
+    return eval(0, nr_token - 1);
 }
+
 
