eff831aae648d2913f49959cf2b6473057314658
tracer-ics2017 <tracer@njuics.org>
2025-03-15 16:28:40 +0800

    >  compile 2212126 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  16:28:40 up  2:04,  1 user,  load average: 0.08, 0.06, 0.01 5e8776ac143a55ce86ee9ceb759266fd1ff142d0

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,8 +38,14 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  TODO();
+  //TODO();
+  op->simm=instr_fetch(eip,op->width);
+  if(op->width==4){
+    op->simm=(int32_t)op->simm;
 
+  }else{
+    op->simm=(int8_t)op->simm;
+  }
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
@@ -309,3 +315,4 @@ void operand_write(Operand *op, rtlreg_t* src) {
   else if (op->type == OP_TYPE_MEM) { rtl_sm(&op->addr, op->width, src); }
   else { assert(0); }
 }
+
