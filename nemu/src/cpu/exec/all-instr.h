459d008bfd57fc881fb113a927e9dda9c546dd8a
tracer-ics2017 <tracer@njuics.org>
2025-03-18 20:54:09 +0800

    >  compile 2212126 zhou Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:54:09 up 2 min,  1 user,  load average: 0.43, 0.31, 0.13 5decf260b52d19373c95c120e3f7121d1a490c27

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -5,9 +5,15 @@ make_EHelper(mov);
 make_EHelper(operand_size);
 make_EHelper(inv);
 make_EHelper(nemu_trap);
-
+//move
 make_EHelper(push);
 make_EHelper(pop);
+make_EHelper(mov);
+make_EHelper(lea);
+make_EHelper(leave);
+make_EHelper(cltd);
+make_EHelper(pusha);
+make_EHelper(popa);
 
 
 
@@ -15,7 +21,36 @@ make_EHelper(pop);
 make_EHelper(call);
 make_EHelper(ret);
 make_EHelper(jmp);
-
-
+make_EHelper(jcc);
+make_EHelper(jmp_rm);
+make_EHelper(call_rm);
+make_EHelper(movsx);
+make_EHelper(movzx);
+make_EHelper(nop);
+//arith
 make_EHelper(sub);
+make_EHelper(add);
+make_EHelper(inc);
+make_EHelper(dec);
+make_EHelper(cmp);
+make_EHelper(neg);
+make_EHelper(adc);
+make_EHelper(sbb);
+make_EHelper(mul);
+make_EHelper(imul1);
+make_EHelper(imul2);
+make_EHelper(imul3);
+make_EHelper(div);
+make_EHelper(idiv);
+
+//logic
 make_EHelper(xor);
+make_EHelper(not);
+make_EHelper(and);
+make_EHelper(or);
+make_EHelper(sal);
+make_EHelper(shl);
+make_EHelper(shr);
+make_EHelper(sar);
+make_EHelper(setcc);
+make_EHelper(test);
